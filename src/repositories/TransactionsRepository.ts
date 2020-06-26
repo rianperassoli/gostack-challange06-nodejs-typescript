7bd3eb1a9041c6a4fcf331869aad19e16eef68a4
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-25 21:52:41 -0300

    Criado método para cadastrar uma categoria

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
@@ -11,7 +11,28 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactionsRepository = getRepository(Transaction);
+
+    const transactions = await transactionsRepository.find();
+
+    const balance = transactions.reduce(
+      (previousValue: Balance, transaction: Transaction) => {
+        const currentBalance = previousValue;
+
+        if (transaction.type === 'income') {
+          currentBalance.income += transaction.value;
+        }
+        if (transaction.type === 'outcome') {
+          currentBalance.outcome += transaction.value;
+        }
+        currentBalance.total = currentBalance.income - currentBalance.outcome;
+
+        return currentBalance;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,48 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: string;
+  category: Category;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    if (!(type === 'income' || type === 'outcome')) {
+      throw new AppError('Invalid transaction type');
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+
+    if (type === 'outcome') {
+      const balance = await transactionsRepository.getBalance();
+
+      const balanceWillBePositive = balance.total > value;
+      if (!balanceWillBePositive) {
+        throw new AppError('The value of outcome is bigger than the balance');
+      }
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: category.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
