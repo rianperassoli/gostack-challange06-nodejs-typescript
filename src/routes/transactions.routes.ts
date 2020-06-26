391a3f9f000263b511dd70d3934be1940c86e738
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-25 23:38:10 -0300

    Criada rota de exclusão de registro

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -34,8 +34,12 @@ transactionsRouter.post('/', async (request, response) => {
   response.json(transaction);
 });
 
-transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+transactionsRouter.delete('/:id', async request => {
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute({ id });
 });
 
 transactionsRouter.post('/import', async (request, response) => {
